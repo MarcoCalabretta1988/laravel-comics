129f224fb391196af8ff97753d8d6528ed922a94
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-23 16:35:56 +0100

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,24 @@
+
+<header>
+        <nav class="container">
+
+            <figure>
+                <a href="#"><img src="{{ Vite::asset('resources/images/dc-logo.png')}}" alt="Dc logo"></a>
+            </figure>
+            <ul>
+                <li > <a href="">CHARACTERS </a></li>
+                   <li  class="current"> <a href="">COMICS</a></li>
+                      <li > <a href="">MOVIES </a></li>
+                         <li > <a href="">TV</a></li>
+                            <li > <a href="">GAMES </a></li>
+                            <li > <a href="">COLLECTIBLES </a></li>
+                         <li > <a href="">VIDEOS</a></li>
+                            <li > <a href="">FANS </a></li>
+                             <li > <a href="">NEWS</a></li>
+                            <li > <a href="">SHOP</a></li>
+            </ul>
+        </nav>
+
+         <section id="jumbotron"></section>
+
+    </header>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends ('layouts.main')
+
+@section ('title', 'Home')
+
+@section ('content')
+
+<h1>Contenuto main</h1>
+@endsection
